134ab65ff18d6aa6a713ed97be2a90557f1b2acd
Michiel Borkent <michielborkent@gmail.com>
2014-09-27 00:30:04 +0200

    Finished animal crud screen

--- src-cljs/webapp/animals.cljs
+++ src-cljs/webapp/animals.cljs
@@ -7,62 +7,104 @@
     [cljs.core.async :refer (<!)]))
 
 (defonce animals-state (atom #{}))
-(defonce initial-form-values {:name ""
-                              :species ""})
-(defonce form-input-state (atom initial-form-values))
 
-(defn add-animal [a]
-  (swap! animals-state conj a)
-  (reset! form-input-state initial-form-values))
-
-(defn remove-animal [a]
-  (swap! animals-state disj a))
-
-(defn field-input-handler
-  "Updates value in form-input-state atom map
-  under key with value from onChange event"
-  [key]
-  (fn [e]
-    (swap! form-input-state
-           assoc key
-           (.. e -target -value))))
-
-(defn are-inputs-valid? []
-  (and (seq (-> @form-input-state :name))
-       (seq (-> @form-input-state :species))))
-
-(defn animal-row [a]
-      [:tr
-       [:td (:name a)]
-       [:td (:species a)
-        [:button.btn.btn-primary.pull-right.btn-danger
-         {:onClick #(remove-animal a)}
-         "\u00D7"]]])
-
+;; fire off go loop only once
 (go (let [response
           (<! (http/get "/animals"))
           data (:body response)]
-      (println "this once")
       (reset! animals-state (set data))))
 
+(defn gen-id []
+  (-> (js/Date.) .valueOf))
+
+(defn add-animal! [a]
+  (swap! animals-state conj (assoc a :id (gen-id))))
+
+(defn remove-animal! [a]
+  (swap! animals-state disj a))
+
+(defn update-animal! [a]
+  (swap! animals-state
+         (fn [old-state]
+           (conj
+             (set (filter (fn [other]
+                            (not= (:id other)
+                                  (:id a)))
+                          old-state))
+             a))))
+
+(defn field-input-handler
+  "Updates value in atom map under key with value from onChange event"
+  [atom key]
+  (fn [e]
+    (swap! atom
+           assoc key
+           (.. e -target -value))))
+
+(defn editable-input [atom key]
+  (if (:editing? @atom)
+    [:input {:type     "text"
+             :value    (get @atom key)
+             :onChange (field-input-handler atom key)}]
+    (get @atom key)))
+
+(defn animal-row [idx a]
+  (let [row-state (atom {:editing? false
+                         :name     (:name a)
+                         :species  (:species a)})
+        current-animal (fn []
+                         (assoc a
+                           :name (:name @row-state)
+                           :species (:species @row-state)))]
+    (fn []
+      [:tr
+       [:td (editable-input row-state :name)]
+       [:td (editable-input row-state :species)]
+       [:td [:button.btn.btn-primary.pull-right
+             {:onClick (fn []
+                         (when (:editing? @row-state)
+                           (update-animal! (current-animal)))
+                         (swap! row-state update-in [:editing?] not))}
+             (if (:editing? @row-state) "Save" "Edit")]]
+       [:td [:button.btn.pull-right.btn-danger
+             {:onClick #(remove-animal! (current-animal))}
+             "\u00D7"]]])))
+
+(defn animal-form []
+  (let [initial-form-values {:name    ""
+                             :species ""}
+        form-input-state (atom initial-form-values)
+        are-inputs-valid? (fn []
+                            (and (seq (-> @form-input-state :name))
+                                 (seq (-> @form-input-state :species))))]
+    (fn []
+      [:tr
+       [:td [:input {:type     "text"
+                     :value    (-> @form-input-state :name)
+                     :onChange (field-input-handler form-input-state :name)}]]
+       [:td [:input {:type     "text"
+                     :value    (-> @form-input-state :species)
+                     :onChange (field-input-handler form-input-state :species)}]]
+       [:td [:button.btn.btn-primary.pull-right
+             {:disabled (not (are-inputs-valid?))
+              :onClick  (fn []
+                          (add-animal! @form-input-state)
+                          (reset! form-input-state initial-form-values))}
+             "Add"]]])))
+
 (defn animals []
-      (fn
-        []
-        [:div
-         [:table.table.table-striped
-          [:thead
-           [:tr
-            [:th "Name"]
-            [:th "Species"]]]
-          [:tbody
-           (map animal-row (sort-by :name @animals-state))
-           [:tr
-            [:td [:input {:type "text"
-                          :value (-> @form-input-state :name)
-                          :onChange (field-input-handler :name)}]]
-            [:td [:input {:type "text"
-                          :value (-> @form-input-state :species)
-                          :onChange (field-input-handler :species)}]
-             [:button.btn.btn-primary.pull-right
-              {:disabled (not (are-inputs-valid?))
-               :onClick #(add-animal @form-input-state)} "Add"]]]]]]))
+  [:div
+   [:table.table.table-striped
+    [:thead
+     [:tr
+      [:th "Name"]
+      [:th "Species"]
+      [:th ""]
+      [:th ""]]]
+    [:tbody
+     (map-indexed
+       (fn [idx a]
+         ^{:key (str "animal-row" (:id a))}
+         [animal-row idx a])
+       (sort-by :name @animals-state))
+     [animal-form]]]])
