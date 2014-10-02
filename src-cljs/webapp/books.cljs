d3900d0dcaa6a5f5ff802a1e417bd1e7df4caa6d
Michiel Borkent <michielborkent@gmail.com>
2014-10-02 16:14:31 +0200

    Removed books screen

--- src-cljs/webapp/books.cljs
+++ src-cljs/webapp/books.cljs
@@ -9,29 +9,12 @@
 
 (defonce books-state (atom []))
 
-(defn book [b]
-      [:div.row
-       [:div.col-sm-4
-        [:img {:src (str "/img/" (:img b))}]]
-       [:div.col-sm-8
-        [:div.panel.panel-default
-         [:div.panel-heading
-          [:h3.panel-title (:title b)]]
-         [:div.panel-body
-          [:p "Authors: " (string/join ", " (:authors b))]
-          [:p "Released: " (.toLocaleDateString (:released b))]
-          [:p "Animal on cover: " (-> b :animal :name)]]]]])
-
 (go (let [response
           (<! (http/get "/books"))
           data (:body response)]
       (reset! books-state (set data))))
 
 (defn books []
-  [:div.container
-   (map (fn [b]
-          ^{:key (str "book-row-" (:id b))}
-          [book b])
-        (sort-by :title @books-state))])
+  [:div.container])
 
 
