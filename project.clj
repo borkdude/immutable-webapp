5bd5c908c2d871289def07893b491ed2a28e1d3a
Martin van Amersfoorth <mamersfo@gmail.com>
2016-01-25 10:39:40 +0100

    Upgrade

--- project.clj
+++ project.clj
@@ -1,31 +1,35 @@
 (defproject immutable-webapp "0.1.0-SNAPSHOT"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2322"]
-                 [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
-                 [ring/ring-core "1.3.1"]
-                 [ring/ring-jetty-adapter "1.3.1"]
-                 [org.webjars/react "0.11.1"]
-                 [org.webjars/bootstrap "3.2.0"]
-                 [cljs-http "0.1.14"]
-                 [compojure "1.1.8"]
-                 [liberator "0.12.0"]
-                 [fogus/ring-edn "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/clojurescript "1.7.228"]
+                 [org.clojure/core.async "0.2.374"]
+                 [ring/ring-core "1.4.0"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [compojure "1.4.0"]
+                 [org.webjars/bootstrap "3.3.6"]
+                 [cljs-http "0.1.39"]
+                 [liberator "0.14.0"]
+                 [fogus/ring-edn "0.3.0"]
                  [clj-json "0.5.3"]
                  [hiccup "1.0.5"]
-                 [reagent "0.4.2"]
-                 [sablono "0.2.22"]]
-  :plugins [[lein-ring "0.8.10"]
-            [lein-cljsbuild "1.0.4-SNAPSHOT"]]
+                 [reagent "0.5.1"]
+                 [prismatic/schema "1.0.4"]
+                 [com.datomic/datomic-free "0.9.5344"]]
+  :plugins [[lein-ring "0.9.7"]
+            [lein-cljsbuild "1.1.2"]]
   :source-paths ["src"]
-  :cljsbuild {
-              :builds [{:id "webapp"
+  :cljsbuild {:builds [{:id "webapp"
                         :source-paths ["src-cljs"]
-                        :compiler {:output-to "resources/public/main.js"
-                                   :output-dir "resources/public/out"
+                        :compiler {:output-to "resources/public/js/main.js"
+                                   :output-dir "resources/public/js"
                                    :optimizations :none
-                                   :source-map true}}]}
+                                   :source-map true
+                                   :parallel-build false
+                                   :asset-path "js"
+                                   :main "webapp.main"}}]}
+  :clean-targets ^{:protect false} [:target-path :compile-path
+                                    "resources/public/js"]
   :ring {:handler webapp.api/handler
          :nrepl {:start? true :port 4500}
          :port 8090}
